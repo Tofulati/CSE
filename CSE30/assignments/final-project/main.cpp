c6a4b71064198934d9617cc8848ff645b50c9ba9
Tofulati <68963772+Tofulati@users.noreply.github.com>
2024-03-13 16:47:30 -0700

    Add files via upload

---
+++ CSE30/assignments/final-project/main.cpp
@@ -0,0 +1,19 @@
+
+// Integration by: Albert, Patrick, Alan, Addy, Michael
+
+#include <cstdlib>
+#include <iostream>
+#include <system_error>
+#include "GameState.h"
+using namespace std;
+
+//0 is player X
+//1 is play O
+
+int main(){
+    GameState game;
+
+    game.start();
+    
+    return 0;
+}
